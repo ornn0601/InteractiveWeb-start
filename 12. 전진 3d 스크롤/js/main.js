f216d3294940a28ab77ce142a75be98a6188ac5f
ornn <info@ornn.co.kr>
2022-02-25 12:37:12 +0900

    스크롤 가로 바, 마우스 좌표 회전

--- 12. 전진 3d 스크롤/js/main.js
+++ 12. 전진 3d 스크롤/js/main.js
@@ -1,15 +1,34 @@
 (function() {
 
+  const stageElem = document.querySelector('.stage');
   const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar');
+  const mousePos = { x: 0, y: 0 };
 
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue = 0;
 
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+  // 스크롤
   window.addEventListener('scroll', function() {
-    const zMove = pageYOffset / maxScrollValue * 980 - 490;
+    const scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
 
+    barElem.style.width = scrollPer * 100 + '%';
   });
 
+  // 마우스 (e.clientX, e.clientY) 
+  window.addEventListener('mousemove', function(e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+  });
+
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
 
 
 })();
